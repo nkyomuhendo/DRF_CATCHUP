99ebd7dc045935b82de5140e038bc1f8a1699118
nkyomuhendo <nkyomuhendo@gmail.com>
2026-09-25 13:12:02 +0300

    Enabled Create or Write Operation of CRUD using the POST Method

--- drinks/views.py
+++ drinks/views.py
@@ -1,18 +1,21 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
-    #get all the drinks 
-    #serialise them
-    #return json
+    
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({"drinks": serializer.data})
 
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    # return JsonResponse(serializer.data, safe=False)
-    # The above returns a list instead of an object and hence the need for the safe = false tag requirment; [{"id": 1, "name": "Grape Soda", "description": "Very Grapey"}, {"id": 2, "name": "Orange Soda", "description": "Very Orangey"}]
-
-    #incase you want an object returned instead of list, do below:
-    return JsonResponse({"drinks": serializer.data})
-    # Here's what it returns: {"drinks": [{"id": 1, "name": "Grape Soda", "description": "Very Grapey"}, {"id": 2, "name": "Orange Soda", "description": "Very Orangey"}]}
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
